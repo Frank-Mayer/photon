129566e531ed0a34f51469afb4ed908b6791ec2c
Frank Mayer <frank.mayer1907@protonmail.com>
2021-07-19 10:04:40 +0200

    Added timing functions
    delay, retriggerableDelay, doOnce

---
+++ src/Timing/Timing.ts
@@ -0,0 +1,42 @@
+import { hashAsync } from "Math/hash";
+
+/**
+ * @async
+ */
+export function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const __retriggerableDelayCache = new Map<number, number>();
+
+/**
+ * Sets a timer that will get reseton
+ * @async
+ */
+export function retriggerableDelay(callback: () => void, ms: number): void {
+  hashAsync(callback.toString()).then((delayId) => {
+    if (__retriggerableDelayCache.has(delayId)) {
+      clearTimeout(__retriggerableDelayCache.get(delayId));
+    }
+    __retriggerableDelayCache.set(delayId, window.setTimeout(callback, ms));
+  });
+}
+
+const __doOnceIdempotency = new Set<number>();
+
+/**
+ * The doOnce function executes a callback only one time
+ * @async
+ */
+export function doOnce<T>(callback: () => T, err?: () => any): Promise<T> {
+  return new Promise((res, rej) => {
+    hashAsync(callback.toString()).then((idempotency) => {
+      if (!__doOnceIdempotency.has(idempotency)) {
+        __doOnceIdempotency.add(idempotency);
+        res(callback());
+      } else if (err) {
+        rej(err());
+      }
+    });
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./DOM/Router";
 export * from "./DOM/DomFrame";
 export * from "./Math/hash";
+export * from "./Timing/Timing";
