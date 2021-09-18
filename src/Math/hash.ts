Frank-Mayer/photon

commit 71c62bed068b4ba47ee66352204844335f4509ad

added sha1 hash (int)

src/Math/hash.ts
@@ -20,3 +20,19 @@ export function hash(
     return hash + 0x80000000;
   }
 }
+
+export async function hashAsync(value: string | { toString: () => string }) {
+  let hash = 0;
+  new Uint32Array(
+    await crypto.subtle.digest(
+      "SHA-1",
+      new TextEncoder().encode(
+        typeof value == "string" ? value : value.toString()
+      )
+    )
+  ).forEach((val) => {
+    hash << 32;
+    hash += val;
+  });
+  return hash;
+}

commit 84ef3010f76762c92555570702fe57f883928adc

fixed issue "expression has no effect"
This alert was introduced in 71c62bed068b4ba47ee66352204844335f4509ad

src/Math/hash.ts
@@ -39,7 +39,7 @@ export async function hashAsync(value: string | { toString: () => string }) {
       )
     )
   ).forEach((val) => {
-    hash << 16;
+    hash <<= 16;
     hash += val;
   });
 